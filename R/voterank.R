e41b9b4768e109c9e1a59c2e564c192415371b77
Viliam Zigo <vilo.zigo@gmail.com>
2022-04-06 09:23:27 +0200

    Lint fix

--- R/voterank.R
+++ R/voterank.R
@@ -1,5 +1,6 @@
 #' @title VoteRank
-#' @description VoteRank algorithm for identifying a set of influential spreaders in complex networks.
+#' @description VoteRank algorithm for identifying a set of influential
+#'              spreaders in complex networks.
 #'
 #' @param g Graph object.
 #' @param r Number of spreaders.
@@ -10,11 +11,20 @@
 #' data(karate)
 #' voterank(karate, 2)
 #'
-#' @references Csardi G, Nepusz T: The igraph software package for complex network research, InterJournal, Complex Systems 1695. 2006. \url{https://igraph.org}
-#' @references Panfeng Liu, Longjie Li, Shiyu Fang, Yukai Yao: Identifying influential nodes in social networks: A voting approach, Chaos, Solitons & Fractals 152. 2021. \url{https://doi.org/10.1016/j.chaos.2021.111309} (\url{https://www.sciencedirect.com/science/article/pii/S0960077921006639})
-#' @references Zhang, JX., Chen, DB., Dong, Q. et al.: Identifying a set of influential spreaders in complex networks. Sci Rep 6, 27823 (2016). \url{https://doi.org/10.1038/srep27823} (\url{https://www.nature.com/articles/srep27823.pdf})
+#' @references Csardi G, Nepusz T: The igraph software package for complex
+#'             network research, InterJournal, Complex Systems 1695. 2006.
+#'             \url{https://igraph.org}
+#' @references Panfeng Liu, Longjie Li, Shiyu Fang, Yukai Yao: Identifying
+#'             influential nodes in social networks: A voting approach, Chaos,
+#'             Solitons & Fractals 152. 2021.
+#'             \url{https://doi.org/10.1016/j.chaos.2021.111309}
+#'             (\url{https://www.sciencedirect.com/science/article/pii/S0960077921006639})
+#' @references Zhang, JX., Chen, DB., Dong, Q. et al.: Identifying a set of
+#'             influential spreaders in complex networks. Sci Rep 6, 27823
+#'             (2016). \url{https://doi.org/10.1038/srep27823}
+#'             (\url{https://www.nature.com/articles/srep27823.pdf})
 voterank <- function(g, r) {
-  if (r <= 0) {
+  if (r <= 0) {S
     stop("Number of spreaders must be positive integer.")
   }
 
@@ -38,7 +48,8 @@ voterank <- function(g, r) {
     for (ith_node in seq_len(graph_order)) {
       neighbours_of_ith_node <- igraph::neighbors(g, ith_node, mode = "out")
       neighbours_indeces <- as.numeric(neighbours_of_ith_node)
-      scores[ith_node] <- ifelse(ith_node %in% spreaders, -1, sum(voting_ability[neighbours_indeces]))
+      scores[ith_node] <- ifelse(ith_node %in% spreaders, -1,
+                                 sum(voting_ability[neighbours_indeces]))
     }
 
     # Identify spreader
