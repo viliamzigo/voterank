9ab275c8caff2020b1784550554fa012633b5113
Viliam Zigo <vilo.zigo@gmail.com>
2022-04-06 08:53:48 +0200

    Documentation update

--- R/voterank.R
+++ R/voterank.R
@@ -1,21 +1,22 @@
-library(igraph)
-library(igraphdata)
-data(karate)
-
-#' VoteRank algorithm
+#' @title VoteRank
+#' @description VoteRank algorithm for identifying a set of influential spreaders in complex networks.
 #'
 #' @param g Graph object.
 #' @param r Number of spreaders.
-#' @export
+#' @return Vector of \code{r} vertices identified as spreaders.
 #' @examples
 #' library(igraphdata)
 #'
 #' data(karate)
 #' voterank(karate, 2)
+#'
+#' @references Csardi G, Nepusz T: The igraph software package for complex network research, InterJournal, Complex Systems 1695. 2006. \url{https://igraph.org}
+#' @references Panfeng Liu, Longjie Li, Shiyu Fang, Yukai Yao: Identifying influential nodes in social networks: A voting approach, Chaos, Solitons & Fractals 152. 2021. \url{https://doi.org/10.1016/j.chaos.2021.111309} (\url{https://www.sciencedirect.com/science/article/pii/S0960077921006639})
+#' @references Zhang, JX., Chen, DB., Dong, Q. et al.: Identifying a set of influential spreaders in complex networks. Sci Rep 6, 27823 (2016). \url{https://doi.org/10.1038/srep27823} (\url{https://www.nature.com/articles/srep27823.pdf})
 voterank <- function(g, r) {
   # Init
   spreaders <- c()
-  graph_order <- gorder(g)
+  graph_order <- igraph::gorder(g)
   if (graph_order < r) {
     stop(
       "Graph order must be greater than number of spreaders."
@@ -31,7 +32,7 @@ voterank <- function(g, r) {
 
     # Calculate scores
     for (ith_node in seq_len(graph_order)) {
-      neighbours_of_ith_node <- neighbors(g, ith_node, mode = 'out')
+      neighbours_of_ith_node <- igraph::neighbors(g, ith_node, mode = "out")
       neighbours_indeces <- as.numeric(neighbours_of_ith_node)
       scores[ith_node] <- sum(voting_ability[neighbours_indeces])
     }
@@ -42,13 +43,10 @@ voterank <- function(g, r) {
 
     # Update voting ability
     voting_ability[spreader] <- 0
-    neighbours_of_spreader <- neighbors(g, spreader, mode = 'out')
+    neighbours_of_spreader <- igraph::neighbors(g, spreader, mode = "out")
     spreader_neighbours_indeces <- as.numeric(neighbours_of_spreader)
-    voting_ability[spreader_neighbours_indeces] <- 1 / mean(degree(g))
+    voting_ability[spreader_neighbours_indeces] <- 1 / mean(igraph::degree(g))
   }
 
-  return(V(g)[spreaders])
+  return(igraph::V(g)[spreaders])
 }
-
-
-voterank(karate, 2)
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# voterank
+# VoteRank
+
 Voterank algorithm for identifying a set of influential spreaders in complex networks.
 
+## Instalation
 
-Sources:
+```r
+install.packages("devtools")
+devtools::install_github("viliamzigo/voterank")
+```
 
-https://www.nature.com/articles/srep27823.pdf
-https://www.sciencedirect.com/science/article/abs/pii/S0960077921006639 ( https://doi.org/10.1016/j.chaos.2021.111309 )
+## References
+
+Csardi G, Nepusz T: The igraph software package for complex network research, InterJournal, Complex Systems 1695. 2006. https://igraph.org
+
+Panfeng Liu, Longjie Li, Shiyu Fang, Yukai Yao: Identifying influential nodes in social networks: A voting approach, Chaos, Solitons & Fractals 152. 2021. https://doi.org/10.1016/j.chaos.2021.111309 (https://www.sciencedirect.com/science/article/pii/S0960077921006639)
+
+Zhang, JX., Chen, DB., Dong, Q. et al.: Identifying a set of influential spreaders in complex networks. Sci Rep 6, 27823 (2016). https://doi.org/10.1038/srep27823 (https://www.nature.com/articles/srep27823.pdf)
